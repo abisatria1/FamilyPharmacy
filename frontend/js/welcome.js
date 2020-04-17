5273da0b6d3f275c7c153e7c2997653d18a13694
Abisatria1 <abi.kadek@gmail.com>
2020-04-18 02:55:58 +0800

    menambahkan file function dan modifikasi fungsi

--- frontend/js/welcome.js
+++ frontend/js/welcome.js
@@ -1,41 +1,17 @@
-
-const cekLogin = () => {
-    const token = sessionStorage.getItem('token');
-    if (!token) window.location = "login.html";
-}
-
 const isiUser = () => {
     const raw = sessionStorage.getItem('user');
     const user = JSON.parse(raw);
     let text = ``;
-    let navbar = ``;
     if (user.statusUser == "0" ){
         // ini pegawai
         text = "Pegawai yang hebat, mulailah hari dengan penuh semangat dan jangan lupa untuk istirahat dan jaga kesehatan dengan baik. SEMANGAT";
-        navbar = 
-        `
-            <a class="nav-item nav-link" href="home.html" id ="active">Beranda</a>
-            <a class="nav-item nav-link" href="order.html">Order</a>
-            <a class="nav-item nav-link" href="Tampilan_obat.html">Obat</a>
-            <a class="nav-item nav-link" href="#">Akun Saya</a>
-            <a class="nav-item nav-link" href="login.html">Log Out</a>
-        `
     }else if (user.statusUser == "1") {
         // ini pemilik
         text = "Pemilik perusahaan yang tercinta. Hari ini sangat istimewa karena penjualan sedang baik";
-        navbar = 
-        `
-            <a class="nav-item nav-link" href="home.html" id ="active">Beranda</a>
-            <a class="nav-item nav-link" href="#">Pegawai</a>
-            <a class="nav-item nav-link" href="order.html">Order</a>
-            <a class="nav-item nav-link" href="Tampilan_obat.html">Obat</a>
-            <a class="nav-item nav-link" href="#">Akun Saya</a>
-            <a class="nav-item nav-link" href="login.html">Log Out</a>
-        `
     }
     $('.nama span').text(user.namaUser);
     $('.paragraf p').text(text);
-    $('.navbar-nav').append(navbar)
+    isiNavbar(user.statusUser);
 }
 
 $(document).ready(function() {
