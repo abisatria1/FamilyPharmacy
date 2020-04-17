368e4e6c317acbb0d82f98d23542afceb8367b6a
Abisatria1 <abi.kadek@gmail.com>
2020-04-18 00:14:27 +0800

    penghapusan folder front end yang sudah dipindahkan ke branch baru

--- frontend/js/welcome.js
+++
@@ -1,9 +0,0 @@
-function isiUser () {
-    const raw = sessionStorage.getItem('user')
-    const user = JSON.parse(raw)
-    $('.nama span').text(user.namaUser)
-}
-
-$(document).ready(function() {
-    isiUser()
-})
