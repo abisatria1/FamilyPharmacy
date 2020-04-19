1e85bc5121c0cd4155231226bbfa9dd4562860cb
Abisatria1 <abi.kadek@gmail.com>
2020-04-19 14:34:32 +0800

    menambahkan create obat

--- frontend/js/obat.js
+++ frontend/js/obat.js
@@ -1,33 +1,34 @@
 const isiDataObat = async () => {
-    try {
-        const token = sessionStorage.getItem('token');
-        const result = await requestDataObat(token);
-        $(result.data.data).each(function(i,item){
-            let string = 
-            `
-            <tr id="row${item.id}">
-            <form action="">
-                <td class="text-center">${i+1}</td>
-                <td class="text-left namaObat">${item.namaObat}</td>
-                <td class="text-center produsenObat">${item.produsenObat}</td>
-                <td class="text-center jenisObat">${item.jenisObat}</td>
-                <td class="text-center hargaObat">${item.hargaObat}</td>
-                <td class="text-center stokObat">${item.stokObat}</td>
-                <td class="text-center">
-                    <a href="#" class="btn badge-success float-center btn-sm" ?>Detail</a>
-                    <a href="" data-izimodal-open="#modal" class="editBtn btn badge-warning float-center btn-sm" onclick="editData(${item.id})"?>Edit</a>
-                    <a href="#" class="btn badge-danger float-center btn-sm" onclick="return confirm('Apakah anda yakin menghapus data ini?');" ?>Hapus</a>
-                </td>
-            </form>
-            </tr>
-            `;
-            $('tbody').append(string);
+    let token = sessionStorage.getItem('token');
+    await requestDataObat(token)
+        .then(result => {
+            $(result.data.data).each(function(i,item){
+                let string = 
+                `
+                <tr id="row${item.id}">
+                <form action="">
+                    <td class="text-center">${i+1}</td>
+                    <td class="text-left namaObat">${item.namaObat}</td>
+                    <td class="text-center produsenObat">${item.produsenObat}</td>
+                    <td class="text-center jenisObat">${item.jenisObat}</td>
+                    <td class="text-center hargaObat">${item.hargaObat}</td>
+                    <td class="text-center stokObat">${item.stokObat}</td>
+                    <td class="text-center">
+                        <a href="#" class="btn badge-success float-center btn-sm" ?>Detail</a>
+                        <a href="" data-izimodal-open="#modal" class="editBtn btn badge-warning float-center btn-sm" onclick="editData(${item.id})"?>Edit</a>
+                        <a href="#" class="btn badge-danger float-center btn-sm" onclick="return confirm('Apakah anda yakin menghapus data ini?');" ?>Hapus</a>
+                    </td>
+                </form>
+                </tr>
+                `;
+                $('tbody').append(string);
+            })
         })
-    } catch (err) {
-        if (err.response.status === 401) {
-            return window.location = 'notAllowed.html';
-        }
-    }
+        .catch(err => {
+            if (err.response.status === 401) {
+                return window.location = 'notAllowed.html';
+            }
+        })
 }
 
 const requestDataObat = token => {
@@ -81,7 +82,31 @@ const editData = id => {
 }
 
 const submitDataObat = async () => {
-    // 
+    let file = $('input[name="fotoObat"]')[0].files[0]
+    let data = new FormData()
+    data.append('namaObat',$('input[name="namaObat"]').val())
+    data.append('jenisObat',$('input[name="jenisObat"]').val())
+    data.append('produsenObat',$('input[name="produsenObat"]').val())
+    data.append('hargaObat',$('input[name="hargaObat"]').val())
+    data.append('stokObat',$('input[name="stokObat"]').val())
+    data.append('fotoObat',file)
+    data.append('descObat',$('textarea[name="descObat"]').val())
+    axios({
+        url : 'http://localhost:3001/drugs/',
+        method : 'POST',
+        headers : {
+            'Authorization' : sessionStorage.getItem('token'),
+            'Accept' : 'multipart/form-data'
+        },
+        data : data
+    })
+    .then(async response => {
+        alert(response.data.message)
+        window.location = 'Tampilan_obat.html'
+    })
+    .catch(err => {
+        alert(err.response.message)
+    })
 }
 
 
@@ -91,9 +116,6 @@ $(document).ready(async function(e) {
     isiNavbar(getUserFromSession().statusUser);
     $(`a[href="home.html"]`).attr('id','');
     $(`a[href="Tampilan_obat.html"]`).attr('id','active');
-    $('#table').DataTable({
-        "lengthMenu" : [[5,10,30,50, -1], [5,10,30,50,"All"]]
-    });
     $('#tambahModal').iziModal({
         title : 'Tambah Data Obat',
         subtitle : 'Diharapkan mengisi data dengan benar dan bertanggung jawab',
@@ -104,4 +126,10 @@ $(document).ready(async function(e) {
             $('#tambahModal input').val('');
         }
     });
+    $('#table').DataTable({
+        "lengthMenu" : [[5,10,30,50, -1], [5,10,30,50,"All"]]
+    });
+    $('#tambahModal form #submit').click(async function(e) {
+        await submitDataObat();
+    })
 })
